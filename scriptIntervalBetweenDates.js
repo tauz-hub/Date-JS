6f171f1c227356d43287544564e156712c88969f
Tauã Ferreira <86664818+tauz-hub@users.noreply.github.com>
2022-03-19 21:55:24 -0300

    Add files via upload

---
+++ scriptIntervalBetweenDates.js
@@ -0,0 +1,23 @@
+Date.prototype.subDays = function (days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() - days);
+  return date;
+}
+
+Date.prototype.addDays = function (days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+const date = new Date();
+const dataBaixa = date.subDays(Math.floor(Math.random() * 365 * 5))
+const modeloAno = dataBaixa.getYear() + 1900
+
+const diffTime = Math.abs(dataBaixa - new Date());
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+const dataFabricacao = dataBaixa.addDays(Math.floor(Math.random() * diffDays))
+
+console.log(modeloAno);
+console.log(dataFabricacao)
