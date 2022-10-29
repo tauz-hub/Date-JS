73aa5d052cbd51f32b00c7c377f01c7011bac984
Tauã Ferreira <86664818+tauz-hub@users.noreply.github.com>
2022-10-29 17:20:21 -0300

    Adding references

--- scriptIntervalBetweenDates.js
+++ scriptIntervalBetweenDates.js
@@ -12,9 +12,9 @@ Date.prototype.addDays = function (days) {
 
 const date = new Date();
 const dataBaixa = date.subDays(Math.floor(Math.random() * 365 * 5))
-const modeloAno = dataBaixa.getYear() + 1900
+const modeloAno = dataBaixa.getYear() + 1900 //adding minimum date
 
-const diffTime = Math.abs(dataBaixa - new Date());
+const diffTime = Math.abs(dataBaixa - new Date()); //difference between dates
 const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
 const dataFabricacao = dataBaixa.addDays(Math.floor(Math.random() * diffDays))
